42e511ae2b0c5cd09dec64c8cc5938f5827b2add
Priyanshusingh3192 <priyanshusingh202010@gmail.com>
2025-04-19 18:02:51 +0530

    added persist

--- client/src/component/A1.js
+++ client/src/component/A1.js
@@ -2,15 +2,24 @@ import React, { useContext, useEffect } from 'react';
 import { User } from '../context/User';
 
 const A1 = () => {
-    const { newUser } = useContext(User);
+    const { newUser,setNewUser} = useContext(User);
 
     useEffect(() => {
         // Extract investorId from localStorage and save it to newUser.userid
         const investorId = localStorage.getItem('investorId');
+        const token = localStorage.getItem('token');
         console.log("Hello ",investorId);
 
-        if (investorId) {
+        if (investorId && token) {
+            setNewUser((old)=>{
+                return({
+                    ...old,
+                    investorId: investorId,
+                    accessToken: token
+                })
+            })
             newUser.userid = investorId;
+            newUser.accessToken = token;
             newUser.investor =  "true";
         }
         console.log("A1 ",newUser);
